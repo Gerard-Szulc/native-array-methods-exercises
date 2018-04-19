24f89d078fbe3028b4f78068ffac403fe12d4b55
Gerard Szulc <gerszulc05@gmail.com>
2018-04-19 22:38:50 +0200

    filter exercise half done

--- filter/core.js
+++ filter/core.js
@@ -1,13 +1,32 @@
 function onlyEven (array) {
-  // your code here
+ return array.filter(function(element){
+   if (element%2===0){
+     return element}
+   })
+
+
 };
 
 function onlyOneWord (array) {
-  // your code here
+  return array.filter(function (element) {
+    if (element === element.split(" ").join("")){
+      return element
+    }
+
+  })
 };
 
 function positiveRowsOnly (array) {
-  // your code here
+return array.filter(function(littleArray){
+
+    return littleArray.filter(function (element) {
+      if (element > 0) {
+        return element;
+      }
+
+    })
+  })
+
 };
 
 function allSameVowels (array) {
